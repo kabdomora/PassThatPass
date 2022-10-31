df3c54935939a1aaeb6337e8eae6566eff9f4445
kabdomora <114969831+kabdomora@users.noreply.github.com>
2022-10-31 15:00:17 -0700

    functions to generate passwords

--- script.js
+++ script.js
@@ -25,112 +25,91 @@ slider.addEventListener("input", update);
 // less likely to produce undesired results from pointing to the wrong subset of string characters
 // within UTF charset
 
+const genFunction = {
+	upper: getRandomUpper,
+	lower: getRandomLower,
+	number: getRandomNumber,
+	special: getRandomSpecial,
+};
+
+
+// above - will use these for constants to refer to functions below
+//below - functions to randomize characters based on selection
+
+function getRandomUpper() {	
+	const uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	return uppers[Math.floor(Math.random() * uppers.length)];
+}
+
+function getRandomLower() {	
+	const lowers = "abcdefghijklmnopqrstuvwxyz";
+	return lowers[Math.floor(Math.random() * lowers.length)];
+}
+
+function getRandomNumber() {	
+	const numbers = "0123456789";
+	return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getRandomSpecial() {	
+	const special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+	return special[Math.floor(Math.random() * special.length)];
+}
+
+
+// need to have a constant set to update the displayed text and read the slider.value
+
+const passwordEl = document.getElementById("password");
+const sliderEl = document.getElementById("slider");
+
+//use this for the call to check checked status
 
 const upperSelected = document.getElementById("uppercase");
 const lowerSelected = document.getElementById("lowercase");
 const numberSelected = document.getElementById("numerical");
 const specialSelected = document.getElementById("special");
 
-const upperEl = upperSelected.checked;
-const lowerEl = lowerSelected.checked;
-const numberEl = numberSelected.checked;
-const specialEl = specialSelected.checked;
 
 
-console.log(upperEl);
-console.log(lowerEl);
-console.log(numberEl);
-console.log(specialEl);
+var generateBtn = document.querySelector("#generate");
+var displayWindow = document.querySelector(".card-body");
 
-
-const passwordEl = document.getElementById("password");
-
-
-function ramdomizeUpper() {	
-	const uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	return uppers[Math.floor(Math.random() * uppers.length)];
-}
-
-function ramdomizeLower() {	
-	const lowers = "abcdefghijklmnopqrstuvwxyz";
-	return lowers[Math.floor(Math.random() * lowers.length)];
-}
-
-function ramdomizeNumber() {	
-	const numbers = "0123456789";
-	return numbers[Math.floor(Math.random() * numbers.length)];
-}
-
-function ramdomizeSpecial() {	
-	const special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-	return special[Math.floor(Math.random() * special.length)];
-}
-
-
-// console.log(lowers);
-// console.log(numbers);
-// console.log(special);
+let generatedPassword = false;
 
 
 
 
-
-
-// will refer to this for the function to update displayed, generated password
-
-
-
-
-// constants for the generator function to refer to for checkbox values 
-// will be ^^ element.value or element.checked
-
-
-// Get references to the #generate element
-
-
-
-
-
-// var generateBtn = document.querySelector("#generate");
-// var selections = document.querySelectorAll("input[type='checkbox']");
-
-// generateBtn.addEventListener("click", function() {
-// 	const sliderEl = +slider.value;
-// 	const upperEl = upperSelected.checked;
-// 	const lowerEl = lowerSelected.checked;
-// 	const numberEl = numberSelected.checked;
-// 	const specialEl = specialSelected.checked;
-// 	passwordEl.innerText = writePassword(sliderEl, upperEl, lowerEl, numberEl, specialEl);
-// });
-
-
-
-
+generateBtn.addEventListener("click", function() {
+	const length = +slider.value;
+	const upperEl = upperSelected.checked;
+	const lowerEl = lowerSelected.checked;
+	const numberEl = numberSelected.checked;
+	const specialEl = specialSelected.checked;
+	generatedPassword = true;
+	passwordEl.innerText = generatePassword(length, upperEl, lowerEl, numberEl, specialEl);
+});
 
 //this is an event listener only it will not generate the password
 
 
 
-
-
-// let generatedPassword = false;
-
-// function generatePassword(sliderEl, randomizeUpper, randomizeLower, randomizeNumber, randomizeSpecial) {
-// 	const checkSelected = randomizeUpper + randomizeLower + randomizeNumber + randomizeSpecial;
-// 	const checkArray = [{ randomizeUpper }, { randomizeLower }, { randomizeNumber }, { randomizeSpecial }].filter(item => Object.values(item)[0]);
-// 	if (checkSelected === 0) {
-// 		return "";
-// 	}
-// 	for (let i = 0; i < sliderEl; i++) {
-// 		checkArray.forEach(type => {
-// 			const generator = Object.keys(type)[0];
-// 			generatedPassword += generate[generator]();
-// 		});
-// 	}
-// 	return generatedPassword.slice(0, length)
-// 	.split('').sort(() => Math.random() - 0.5)
-// 	.join('');
-// 	}
+function generatePassword(length, upper, lower, number, special) {
+	let generatedPassword = "";
+	const checkSelected = upper + lower + number + special;
+	const checkArray = [{ upper }, { lower }, { number }, { special }].filter(item => Object.values(item)[0]);
+	if (checkSelected === 0) {
+		return "";
+	}
+	for (let i = 0; i < length; i++) {
+		checkArray.forEach(type => {
+			const generator = Object.keys(type)[0];
+			generatedPassword += genFunction[generator]();
+		});
+	}
+	return generatedPassword.slice(0, length)
+					.split('').sort(() => Math.random() - 0.5)
+					.join('');
+	}
 
 	
 
